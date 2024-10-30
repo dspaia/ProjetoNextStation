a0fe960b84c50cbe2d18b76d6f9268349095c888
dspaia <campos.paiva@aluno.ifsp.edu.br>
2024-10-29 22:01:56 -0300

    cronometro e cena de credito

---
+++ addons/ATTRIBUTION.md
@@ -0,0 +1,25 @@
+# Attribution
+## Next Station
+
+
+
+### Creators
+
+Adriano Monteagudo
+Gabriel Paiva
+Júlia Calixto
+Luana Melo
+
+
+
+## Tools
+
+#### Godot
+ 
+
+
+#### Godot Credits Scene
+
+Author: [Marek Belski](https://github.com/Maaack/Godot-Credits-Scene/graphs/contributors)  
+Source: [github: Godot-Credits-Scene](https://github.com/Maaack/Godot-Credits-Scene)  
+License: [MIT License](LICENSE.txt)  
--- scripts/hud_manager.gd
+++ scripts/hud_manager.gd
@@ -4,15 +4,15 @@ extends Control
 @onready var contadortime = $container/timercontainer/contadortime as Label
 @onready var clock_timer = $clock_timer as Timer
 
-var minutos = 9
+var minutos = 0
 var segundos = 0
-@export_range(0, 23) var default_minutos := 9
-@export_range(0, 59) var default_segundos := 0
+@export_range(0, 23) var default_minutos := 0  # Tempo inicial em minutos
+@export_range(0, 59) var default_segundos := 5  # Tempo inicial em segundos
+@export var scene_to_load: String = "res://cenas/conceicao"  # Caminho da nova cena
 
 # Chamado quando o nó entra na árvore da cena pela primeira vez
 func _ready():
 	contadormoeda.text = str(Tudo.moeda)
-	contadortime.text = str("%02d" % default_minutos) + ":" + str("%02d" % default_segundos)
 	reset_clock_timer()
 
 # Chamado a cada frame
@@ -21,15 +21,15 @@ func _process(delta):
 
 # Chamado quando o clock_timer dá timeout (a cada intervalo definido)
 func _on_clock_timer_timeout():
-	segundos += 0.1  # Incrementar segundos
+	if segundos == 0:  # Se os segundos chegaram a 0
+		if minutos > 0:  # Se ainda há minutos
+			minutos -= 1
+			segundos = 59  # Reseta os segundos para 59
+		else:
+			end_game()  # Se não há mais minutos, encerra o jogo
+	else:
+		segundos -= 1  # Decrementa os segundos
 
-	if segundos == 60:  # Se chegou a 60 segundos
-		minutos += 1
-		segundos = 0  # Reseta os segundos
-		
-	if minutos == 24:  # Se chegou a 24 minutos, reseta para 0 (ou faça outra ação)
-		minutos = 0  # Reseta os minutos
-	
 	# Atualiza o display do timer
 	contadortime.text = str("%02d" % minutos) + ":" + str("%02d" % segundos)
 
@@ -42,3 +42,10 @@ func reset_clock_timer():
 	# Certifique-se de que o timer está configurado para acionar a cada 1 segundo
 	clock_timer.wait_time = 1  # Define o tempo de espera como 1 segundo
 	clock_timer.start()  # Inicia o timer
+
+# Função para encerrar o jogo
+func end_game():
+	if segundos == 0 and minutos == 0:
+		# Muda para a nova cena
+		print("Mudando para a nova cena!")
+		get_tree().change_scene_to_file("res://addons/scenes/end_credits/end_credits.tscn")  # Altera a cena para a especificada
